be5adf52c9557f8ce910dce3c7c25a370d46bdf8
Bojan Vujovic <Bojan Vujovic>
2016-09-27 23:02:56 +0200

    ver 1

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'moja-aplikacija',
+    templateUrl: 'app/app.component.html'
+    // template: '<h1>Moja prva Angular 2 Aplikacija :)</h1>'
+})
+export class AppComponent { }
