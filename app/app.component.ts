551128c3afc3a1f20f8178c2b8b7f0abb6766784
Bojan Vujovic <Bojan Vujovic>
2016-09-27 23:36:07 +0200

    ver 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
 import {Component} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx';
+
 @Component({
     selector: 'moja-aplikacija',
     templateUrl: 'app/app.component.html'
-    // template: '<h1>Moja prva Angular 2 Aplikacija :)</h1>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+
+    korisnici: Object[];
+
+    constructor(http: Http) {
+        http.get('korisnici.json')
+            .map(res => res.json())
+            .subscribe(korisnici => this.korisnici = korisnici);
+    }
+}
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-bootstrap(AppComponent);
+import { bind } from 'angular2/core';
+import { Http, Headers, HTTP_PROVIDERS } from 'angular2/http';
+
+bootstrap(AppComponent, [HTTP_PROVIDERS]);
